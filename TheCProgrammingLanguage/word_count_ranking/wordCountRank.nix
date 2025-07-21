daa9cb8b2d266cd04acb0e62e15afb8d5375fb37
thesecretlifeofabunny <212422141+thesecretlifeofabunny@users.noreply.github.com>
2025-07-20 20:33:34 -0700

    6-4 added

---
+++ TheCProgrammingLanguage/word_count_ranking/wordCountRank.nix
@@ -0,0 +1,20 @@
+# wordCountRanking.nix
+
+let
+  pkgs = import <nixpkgs> { };
+in
+  pkgs.stdenv.mkDerivation {
+    name = "word_count_rank";
+
+    src = ./.;
+
+    buildPhase = ''
+      $CC word_count_rank.c -o word_count_rank
+    '';
+
+	installPhase = ''
+		mkdir -p $out/bin
+		cp word_count_rank $out/bin/word_count_rank
+	'';
+  }
+
---
+++ TheCProgrammingLanguage/word_count_ranking/word_count_rank.c
@@ -0,0 +1,12 @@
+/*
+Exercise 6-4. Write a program that prints the distrinct words in its input
+sorted into descreasing order of frequency of occurance.
+precede each word by its count.
+*/
+
+#include <stdio.h>
+
+int main (void) {
+    printf("word count rank\n");
+    return 0;
+}
