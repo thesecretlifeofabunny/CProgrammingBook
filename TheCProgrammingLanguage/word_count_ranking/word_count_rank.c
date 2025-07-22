c96b40cb4af13fd9a3a149fb491f1859544ddd59
thesecretlifeofabunny <212422141+thesecretlifeofabunny@users.noreply.github.com>
2025-07-21 19:26:18 -0700

    todo added

--- TheCProgrammingLanguage/word_count_ranking/word_count_rank.c
+++ TheCProgrammingLanguage/word_count_ranking/word_count_rank.c
@@ -2,6 +2,16 @@
 Exercise 6-4. Write a program that prints the distrinct words in its input
 sorted into descreasing order of frequency of occurance.
 precede each word by its count.
+
+TODO:
+
+- I think hashmap
+    - Hashmap will either be hashmap of the below struct first fit, or linked list on each fit and append to list at hash spot.
+- Need to do a hash function
+- struct of word + count attributes
+- for each word add word to map update count if hash hit + word match
+- seperate intake by punctation !?,. and spaces
+- print out function
 */
 
 #include <stdio.h>
